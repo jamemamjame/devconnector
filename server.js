a2cf009d8c4147ffd9ce442de7aa949f5d975fe0
jamemamjame <jamemamjame@gmail.com>
2019-04-26 01:15:54 +0700

    Passport JWT Authentication Strategy

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /** Alternative method to connect mongo Atlas
 // DB config
@@ -24,18 +25,25 @@ client.connect()
     .catch(err => console.log(err))
  */
 
-const connectionString = require('./config/keys').mongoURI;
-mongoose.connect(connectionString, { useNewUrlParser: true })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err))
+const connectionString = require("./config/keys").mongoURI;
+mongoose
+  .connect(connectionString, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!!'))
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport.js")(passport);
+
+// app.get("/", (req, res) => res.send("Hello World!!"));
 
 // Use Routes
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
