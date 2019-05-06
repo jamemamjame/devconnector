66501e35a1f61d24f6e6304bfe72c9c669705a33
jamemamjame <jamemamjame@gmail.com>
2019-05-06 10:30:23 +0700

    fix auth return null for existing user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
+const User = require("../../models/User");
+
 // @route       GET api/auth
 // @desc        Tests route
 // @access      Public
