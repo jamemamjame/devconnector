3ff1b002de408d3325cfe5b6d49aa93a2483c10e
jamemamjame <jamemamjame@gmail.com>
2019-05-28 01:18:23 +0700

    Finish add `Edit Profile` page

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,47 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
+
+    if (!edit) {
+      // if not edit (is new create), we will go to `/dasboard`. We cannot use redirect which is a React component in here
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    console.log(err);
+
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => {
+        dispatch(setAlert(error.msg, "danger"));
+      });
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
