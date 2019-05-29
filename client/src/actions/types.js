7bffd951ffe29a8b5890cbf16f9139d7cee49d63
jamemamjame <jamemamjame@gmail.com>
2019-05-29 18:01:13 +0700

    Delete Edu,Exp,Account

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,5 @@
-// action.type
+// StringKey for action.type
+
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 
@@ -14,5 +15,7 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 
 export const GET_PROFILE = "GET_PROFILE";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const CLEAR_PROFILE = "CLEAR_PROFILE";
 export const PROFILE_ERROR = "PROFILE_ERROR";
+export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
